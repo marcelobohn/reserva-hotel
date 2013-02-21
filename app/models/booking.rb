8e39be3dfa8269401b86f143a654eccb77c0957e
marcelobohn <marcelo.bohn@gmail.com>
2013-02-20 21:50:22 -0300

    ajusta de lista de reservas

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,12 +10,20 @@ class Booking < ActiveRecord::Base
 # from rooms r 
 # left join bookings b on r.id = b.room_id and cast(b.entry as date) between '#{data_ini}' and '#{data_fim}'
 # group by r.id, r.name}
-    sql = %{select r.id, r.name, max(b.id) booking_id, count(b.id) booking_count
+#     sql = %{select r.id, r.name, max(b.id) booking_id, count(b.id) booking_count
+# from rooms r 
+# left join bookings b on r.id = b.room_id and 
+# ('#{data_ini}' between cast(b.entry as date) and cast(b.exit as date) 
+# or '#{data_fim}' between cast(b.entry as date) and cast(b.exit as date))
+# group by r.id, r.name}
+    sql = %{select r.id, r.name, max(b.id) booking_id, count(b.id) booking_count, r.floor_id, f.building_id
 from rooms r 
+left join floors f on f.id = r.floor_id
 left join bookings b on r.id = b.room_id and 
 ('#{data_ini}' between cast(b.entry as date) and cast(b.exit as date) 
 or '#{data_fim}' between cast(b.entry as date) and cast(b.exit as date))
-group by r.id, r.name}
+group by r.id, r.name, r.floor_id, f.building_id
+order by building_id, floor_id}
 
     return ActiveRecord::Base.connection.execute(sql).to_a
 
